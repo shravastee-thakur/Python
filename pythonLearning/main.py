fbec468e138eae499ec7ecbed113cbbbcb218773
Shravastee Thakur <123631680+shravastee-thakur@users.noreply.github.com>
2023-12-03 17:32:22 +0530

    Add files via upload

---
+++ pythonLearning/main.py
@@ -0,0 +1,11 @@
+print("Hello, World!", 1, 2, 3, sep="-", end="!!! \n")
+print("goodbye")
+
+age = 23
+print(age)
+
+a, b, c = "apple", "banana", "coconut"
+print(a, b, c)
+
+my_name = input("Enter your name: ")
+print(f"Hello, {my_name}")
---
+++ pythonLearning/numbers.py
@@ -0,0 +1,17 @@
+result = abs(-5.3)  # absolute value
+print(result)
+
+minimum = min(1, 2, 3, 4)
+print(minimum)
+
+maximum = max([23, 21, 27])
+print(maximum)
+
+add = sum([1, 2, 3], 4)
+print(add)
+
+power = pow(5, 3, 17)  # 125 / 17
+print(power)
+
+ans = divmod(26, 7)  # provides quotient and remainder
+print(ans)
